e09b00bbf7b935af64df98f9410d224f0554e2c1
Gary Wheeler <gkwheeler40@gmail.com>
2019-03-27 15:44:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@ board = [" "," "," "," ","x"," "," "," "," "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |#{board[5]}|   "
+  puts "   |#{board[5]}  |   "
   puts "-----------"
   puts "   |   |   "
 end
